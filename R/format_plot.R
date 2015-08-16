516d67157184ae1a6529a851d7a388c32b45df5f
Ricardo Vladimiro <r.vladimiro@gmail.com>
2015-08-16 11:52:53 +0100

    Cleaned and commented format_plot

--- R/format_plot.R
+++ R/format_plot.R
@@ -1,11 +1,14 @@
+# Helper function to format plots
+format_plot <- function(plot,                # The plot to format
+                        title = NULL,        # Plot title
+                        x_label = NULL,      # Label of x axis
+                        y_label = NULL,      # Label of y axis
+                        color_title = NULL,  # Legend title for color var
+                        size_title = NULL,   # Legend title for size var
+                        facet_formula = NULL # The formula to create facets
+                        ) {
 
-format_plot <- function(plot,
-                        title = NULL,
-                        x_label = NULL,
-                        y_label = NULL,
-                        color_title = NULL,
-                        size_title = NULL,
-                        facet_formula = NULL) {
+  # Adds titles and labels
 
   if (!is.null(title)) {
     plot = plot + ggtitle(paste0(title, '\n'))
@@ -28,27 +31,33 @@ format_plot <- function(plot,
   }
 
 
+
+  # If there's a facet formula the plot is formated to have a centered title
+  # and the panel borders are not removed to keep the several plots clearly
+  # separated.
   if(!is.null(facet_formula)) {
     plot = plot +
-      facet_grid(facet_formula) +
-      theme_bw() +
-      theme(
-        legend.position = 'bottom',
-        plot.title =
-          element_text(family = 'Trebuchet MS', face = 'bold', size = 16))
+           facet_grid(facet_formula) +
+           theme_bw() +
+           theme(legend.position = 'bottom',
+                 plot.title = element_text(family = 'Trebuchet MS',
+                                           face = 'bold',
+                                           size = 16))
+  # If there isn't a facet formula, the panel borders are removed and axis
+  # lines are drawn in black. The title is aligned to the right.
   } else {
     plot = plot +
-      theme_bw() +
-      theme(
-        axis.line = element_line(colour = "black"),
-        legend.position = 'bottom',
-        panel.border = element_blank(),
-        panel.background = element_blank(),
-        plot.title =
-          element_text(hjust = 1, family = 'Trebuchet MS', face = 'bold', size = 16)
-      )
-
+           theme_bw() +
+           theme(axis.line = element_line(colour = "black"),
+                 legend.position = 'bottom',
+                 panel.border = element_blank(),
+                 panel.background = element_blank(),
+                 plot.title = element_text(hjust = 1,
+                                           family = 'Trebuchet MS',
+                                           face = 'bold',
+                                           size = 16))
   }
 
+  # Returns the formatted plot
   return(plot)
 }
